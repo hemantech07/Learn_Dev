b33f17592efc1ecc1050dc813aa6bff2eb4ce0bb
hemant <hgupta2782@gmail.com>
2021-06-10 03:16:55 +0530

    aysncSerial nFiles Callback

--- Callbacks/nFiles/asyncSerial.js
+++ Callbacks/nFiles/asyncSerial.js
@@ -2,6 +2,7 @@ let fs = require('fs');
 
 let files = ["../text/f1.txt" , "../text/f2.txt" , "../text/f3.txt"];
 
+// Recursive Approach: PASS
 output(0);
 
 function output(i) {
@@ -10,5 +11,16 @@ function output(i) {
     fs.readFile(files[i], function(err, data) {
         console.log(data+"");
         output(i+1);
+    })
+}
+
+// Iterative Approach: FAIL
+// (Maybe stuck in event loop)
+let i=0;
+
+while (i<files.length) {
+    fs.readFile(files[i], function(err, data) {
+        console.log(data+"");
+        i+=1;
     })
 }
